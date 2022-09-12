d08cd8006d67858d39cb8464d778a9f17919c50f
EcoFriendlyAppleSu <40160510+EcoFriendlyAppleSu@users.noreply.github.com>
2022-09-12 14:58:39 +0900

    chore : Project Init (#1)

---
+++ app/src/main/java/org/healthplus/HealthPlusApplication.java
@@ -0,0 +1,11 @@
+package org.healthplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthPlusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HealthPlusApplication.class, args);
+    }
+}
