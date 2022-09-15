28fa927f84bcca6d59c101fa41ac48e61fa18909
EcoFriendlyAppleSu <40160510+EcoFriendlyAppleSu@users.noreply.github.com>
2022-09-15 20:25:51 +0900

    Add modules (#3)
    Co-authored-by: vellatrix <106750536+vellatrix@users.noreply.github.com>

---
+++ delivery-worker/src/main/java/org/healthplus/DeliveryWorkerApplication.java
@@ -0,0 +1,11 @@
+package org.healthplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryWorkerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryWorkerApplication.class, args);
+    }
+}
---
+++ vendor/src/main/java/org/healthplus/VendorApplication.java
@@ -0,0 +1,11 @@
+package org.healthplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VendorApplication.class, args);
+    }
+}
