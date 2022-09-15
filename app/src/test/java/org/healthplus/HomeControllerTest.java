2530b875c2bea8dd6dee01719e4aabda0c1b8e65
EcoFriendlyAppleSu <40160510+EcoFriendlyAppleSu@users.noreply.github.com>
2022-09-15 20:24:14 +0900

    Modify git ignore and add tempalte (#2)

---
+++ app/src/test/java/org/healthplus/HomeControllerTest.java
@@ -0,0 +1,24 @@
+package org.healthplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class HomeControllerTest {
+
+    @Test
+    public void homeControllerTest(@Autowired MockMvc mvc) throws Exception {
+        mvc.perform(get("/")).andExpect(status().isOk())
+                .andExpect(content().string("Greeting From Spring Boot!"));
+    }
+}
