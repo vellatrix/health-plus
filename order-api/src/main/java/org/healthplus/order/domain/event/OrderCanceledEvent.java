af703d0f195b98531f761f89d41e4431be549778
vellatrix <siryou10@naver.com>
2022-12-24 21:31:40 +0900

    Fix : OrderPayment Logic fix

--- order-api/src/main/java/org/healthplus/order/domain/event/OrderCanceledEvent.java
+++ order-api/src/main/java/org/healthplus/order/domain/event/OrderCanceledEvent.java
@@ -1,10 +1,13 @@
 package org.healthplus.order.domain.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.healthplus.model.domain.DomainEvent;
 
 @Getter
+@AllArgsConstructor
 public class OrderCanceledEvent extends DomainEvent {
 
   private Long orderId;
+
 }
