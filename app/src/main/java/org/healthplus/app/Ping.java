3f169ea2149045c5d34c74e1486ee367a3789193
vellatrix <siryou10@naver.com>
2023-01-07 23:43:57 +0900

    Github Action Test

--- app/src/main/java/org/healthplus/app/Ping.java
+++ app/src/main/java/org/healthplus/app/Ping.java
@@ -1,13 +1,16 @@
 package org.healthplus.app;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class Ping {
 
   @GetMapping("/ping")
   public String answer() {
+    log.info("Ping is successful");
     return "pong";
   }
 }
